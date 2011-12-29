274d697121f1c40b7871e7f22747f9f5a77d3ac1
Adam Dalton <adam.dalton@gmail.com>
2011-12-29 11:02:32 -0500

    posts are now added to a blog and displayed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+
   attr_accessor :blog, :title, :body
 
   def initialize(attrs={})
@@ -9,5 +12,9 @@ class Post
 
   def publish
     blog.add_entry(self)
+  end
+
+  def persisted?
+    false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails32Bootstrap::Application.routes.draw do
   get "blog/index"
 
   root to: "blog#index"
+  resources :posts
 end
